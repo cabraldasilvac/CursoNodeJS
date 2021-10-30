fd6103b99ebc3d5b0fe61a6c6f44d6e2b5aed0c7
WANDERLEY C SILVA <39313251+cabraldasilvac@users.noreply.github.com>
2021-10-30 13:40:12 -0300

    Add HTTP

--- http/server.js
+++ http/server.js
@@ -2,14 +2,12 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
-
 http.createServer((req, res) => {
 
     const file = req.url === '/' ? 'index.html' : req.url
     const filePath = path.join(__dirname, 'public', file)
     const extname = path.extname(filePath)
-
-    const allowedFileTypes = ['.html', '.css', 'js']
+    const allowedFileTypes = ['.html', '.css', '.js']
     const allowed = allowedFileTypes.find(item => item == extname)
 
     if (!allowed) return
