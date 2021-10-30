867a4704a1d3dc5c13412690c2fcf4ec9fe70ac6
WANDERLEY C SILVA <39313251+cabraldasilvac@users.noreply.github.com>
2021-10-30 12:16:57 -0300

    end lloger

--- js/logger.js
+++ js/logger.js
@@ -1,3 +1,4 @@
+// projeto que fica "ouvindo" o que acontece[log] e escreve em outro arquivo. [log.txt]
 const EventEmitter = require('events')
 const fs = require('fs')
 const path = require('path')
--- js/stats.js
+++ js/stats.js
@@ -3,7 +3,6 @@ const os = require('os')
 const log = require('./logger')
 setInterval(() => {
         const { freemem, totalmem } = os
-
         const total = parseInt(totalmem() / 1024 / 1024)
         const mem = parseInt(freemem() / 1024 / 1024)
         const percents = parseInt((mem / total) * 100)
